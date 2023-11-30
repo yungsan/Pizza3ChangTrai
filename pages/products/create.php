01ab17137377f4c503afdabbd6cd480936ed57ef
solmak8980 <kaynguyen22898@gmail.com>
2023-11-30 08:58:28 +0700

    update page product

--- pages/products/create.php
+++ pages/products/create.php
@@ -1,26 +1,27 @@
 
-<section class="h-80 bg-light mt-5">
-  <div class="container py-5 h-100">
-    <div class="row d-flex justify-content-center align-items-center h-100">
+<section class="h-80 bg-light mt-5 ">
+  <div class="container py-5 h-80">
+    <div class="row d-flex justify-content-center align-items-center h-80">
       <div class="col">
         <div class="card card-registration my-4">
-          <form action="">
-          <div class="row g-0">
-            <div class="col-xl-5 d-none d-xl-block">
+          <form action="pages/products/update.php" method="POST" enctype="multipart/form-data" autocomplete="off">
+          <div class="row g-0 ">
+            <div class="col-xl-4 d-none d-xl-block">
                 <div class="ecommerce-gallery" data-mdb-zoom-effect="true" data-mdb-auto-height="true">
                     <div class="row py-3 shadow-5 mx-2">
                         <div class="col-12 mb-2">
                             <div class="lightbox">
                                 <img
-                                src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/14a.webp"
+                                src="assets/images/blank_img.webp"
                                 alt="Gallery image 1"
-                                class="ecommerce-gallery-main-img active w-100"
+                                class="ecommerce-gallery-main-img active w-100 rounded" id="thumnail" onclick="triggerFileInput()"
                                 />
+                                <input type="file" name="productImage[]" id="inputImage" hidden>
                             </div>
                         </div>
                         <div class="col-3 mt-1">
                             <img
-                                src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/14a.webp"
+                                src="assets/images/blank_img.webp"
                                 data-mdb-img="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/14a.webp"
                                 alt="Gallery image 1"
                                 class="active w-100"
@@ -28,7 +29,7 @@
                         </div>
                         <div class="col-3 mt-1">
                             <img
-                                src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/12a.webp"
+                                src="assets/images/blank_img.webp"
                                 data-mdb-img="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/12a.webp"
                                 alt="Gallery image 2"
                                 class="w-100"
@@ -36,7 +37,7 @@
                         </div>
                         <div class="col-3 mt-1">
                             <img
-                                src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/13a.webp"
+                                src="assets/images/blank_img.webp"
                                 data-mdb-img="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/13a.webp"
                                 alt="Gallery image 3"
                                 class="w-100"
@@ -44,7 +45,7 @@
                         </div>
                         <div class="col-3 mt-1">
                             <img
-                                src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/15a.webp"
+                                src="assets/images/blank_img.webp"
                                 data-mdb-img="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/15a.webp"
                                 alt="Gallery image 4"
                                 class="w-100"
@@ -53,21 +54,20 @@
                     </div>
                     </div>
                 </div>
-                <div class="col-xl-7">
+                <div class="col-xl-8">
                 <div class="card-body p-md-4 text-black">
-                    <h3 class="mb-4 text-uppercase">Tạo sản phẩm</h3>
-
+                    <h4 class="mb-4 text-uppercase">Tạo sản phẩm</h4>
                     <div class="row">
                         <div class="col-md-6 mb-4">
                             <div class="form-outline">
-                            <label class="form-label" for="form3Example1m">Tên sản phẩm</label>
-                            <input type="text" id="form3Example1m" class="form-control form-control-lg" placeholder="Nhập tên sản phẩm" />
+                            <label class="form-label" for="productName">Tên sản phẩm</label>
+                            <input type="text" name="productName" id="productName" class="form-control form-control-lg" placeholder="Nhập tên sản phẩm" />
                             </div>
                         </div>
                         <div class="col-md-6 mb-4">
-                            <label class="form-label" for="form3Example1m">Đơn giá</label>
+                            <label class="form-label" for="productPrice">Đơn giá</label>
                             <div class="input-group input-group-lg">
-                                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" placeholder="Nhập giá tiền">
+                                <input type="text" name="productPrice" id="productPrice" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" placeholder="Nhập giá tiền">
                                 <span class="input-group-text" id="inputGroup-sizing-lg">VNĐ</span>
                             </div>
                         </div>
@@ -117,8 +117,8 @@
                         </div>
                     </div>
                     <div class="input-group">
-                        <span class="input-group-text">With textarea</span>
-                        <textarea class="form-control" aria-label="With textarea"></textarea>
+                        <span class="input-group-text">Chi tiết</span>
+                        <textarea name="ChiTietSanPham" id="ChiTietSanPham" class="form-control" aria-label="With textarea"></textarea>
                     </div>
                     <div class="d-flex justify-content-end pt-3">
                     <button type="submit" class="btn btn-warning btn-lg ms-2">Cập nhật sản phẩm</button>
@@ -131,4 +131,22 @@
       </div>
     </div>
   </div>
+
+  <script>
+    function triggerFileInput() {
+    document.getElementById('inputImage').click();
+    }
+
+    document.getElementById('inputImage').addEventListener('change', function(event) {
+    var input = event.target;
+    var reader = new FileReader();
+
+    reader.onload = function(){
+        var image = document.getElementById('thumnail');
+        image.src = reader.result;
+    };
+
+    reader.readAsDataURL(input.files[0]);
+    });
+</script>
 </section>
