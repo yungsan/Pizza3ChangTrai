0fb9ae79e0998703b455aa94d05d4360c9a867ea
solmak8980 <kaynguyen22898@gmail.com>
2023-12-06 18:15:25 +0700

    fix cart page

--- pages/cart/index.php
+++ pages/cart/index.php
@@ -1,66 +1,37 @@
 <section>
     <div class="container-fluid page-header mb-5" style="background: none">
-        <div class="container mx-5">
+        <div class="container">
           <!-- cart header -->
           <!-- Cart body -->
-          <div class="row d-flex justify-content-center  mt-2">
-            <div class="col-10 bg-light border rounded mx-4 p-3">              
+          <div class=" row d-flex justify-content-center">
+            <div class="col-10 bg-light shadow-lg p-3 mb-5 bg-body rounded">
+              
               <div class="product_row">
                 <!-- Product1 -->
-
                 <?php
                   require_once('config/database.php');
-                  $sql = "SELECT*FROM carts";
+                  $sql = "SELECT * FROM carts";
                   $result = $connect->query($sql);
-                  $row = $result->fetch_assoc();
-                ?>
-                <div class="card_row card rounded-3 mb-4">
+                  if (!$result) {
+                      echo "Error";
+                      die();
+                  }
+                  while($row = $result->fetch_assoc())
+                  echo'<div class="card_row card rounded-3 mb-4">
                   <div class="card-body p-4">
                     <div class="row d-flex justify-content-between align-items-center">
                       <div class="col-md-2 col-lg-2 col-xl-2">
                         <a href="?page=checkout">
-                        <!-- <img
-                          src="https://via.placeholder.com/200x150"
-                          class="img-fluid rounded-3" alt="Piza1"> -->
-                          <?php
-                            $images1 = $row['thumbnail'];
-                            echo '<img style="object-fit: cover;" width="200" height="150" alt="hinh san pham" class="img-fluid rounded-3" src="'."pages/admin/pages/products/". $images1.'"/>';
-                          ?>
+                        <img class="img-fluid rounded-3" src="pages/admin/pages/products/' . $row['thumbnail'] . '" alt="thumbnail">
                         </a>
                       </div>
                       <div class="col-md-3 col-lg-3 col-xl-3">
-                        <a href="?page=checkout"><p class="lead fw-normal mb-2">
-                          <?php
-                            echo'<h5 class="">
-                              '.$row['product_name'].'';
-                            echo'</h5>';
-                          ?>
-                        </p></a>
-                        <p><span class="text-muted">Size: </span>L <span class="text-muted">
-                      </div>
-                      <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
-                        <button class="minus btn btn-link px-2"
-                          onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
-                          <i class="fas fa-minus"></i>
-                        </button>
-
-                        <input id="form1" min="1" name="quantity" value="1" type="number"
-                          class="qty form-control form-control-sm" />
-
-                        <button class="plus btn btn-link px-2"
-                          onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
-                          <i class="fas fa-plus"></i>
-                        </button>
+                        <a href="?page=checkout"><p class="lead fw-normal mb-2">' . $row['product_name'] . '</p></a>
+                        <p><span class="text-muted">Size: </span>' . $row['size'] . '<span class="text-muted">
                       </div>
                       <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
                         <!-- <input type="number" value="9" class="price form-control form-control-sm" disabled> -->
-                        <span class="price mb-0">
-                          <?php
-                            echo'<span class="act-price">
-                              '.$row['price'].'đ';
-                            echo'</span>';
-                          ?>
-                        </span>
+                        <span class="price mb-0">' . number_format($row['price'], 0, '.', '.') . ' đ</span>
                         <!-- <h5 class="price mb-0">$9.00</h5> -->
                       </div>
                       <div class="col-md-1 col-lg-1 col-xl-1 text-end mr-4">
@@ -69,9 +40,11 @@
                     </div>
                   </div>
                 </div>
+                <hr/>
+                ';
+                ?>
+                <!-- end proodct1 -->
               </div>
-
-
               <!-- Cart footer -->
               <div class="card mb-4">
                 <div class="card-body p-4 d-flex flex-row">
@@ -86,14 +59,12 @@
                 <div class="card-body position-relative">
                   <span class="h4">Order total:$</span>
                   <!-- <span class="lead fw-normal h5">$</span> -->
-                  <span class="total h4">14</span>
-                  <button type="button" class="btn btn-warning btn-block btn-lg position-absolute top-0 end-0 m-2">Proceed to Pay</button>
+                  <span class="total h4">XXXX</span>
+                  <button type="button" class="btn btn-warning btn-block btn-lg position-absolute top-0 end-0 m-2">Thanh Toán</button>
                 </div>
               </div>
             </div>
           </div>
         </div>
     </div>
-    <!-- <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script> -->
-    <script src="pages/cart/updatetotal.js" ></script>
 </section>
